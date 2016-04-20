ae1802881f1e2c9a2357b8748e5bf72c5145f81a
Adly Abdullah <adly@trustedcompany.com>
2016-04-20 17:03:18 +0800

    Formatting

--- s16/c16/s16.c
+++ s16/c16/s16.c
@@ -3,20 +3,20 @@
 #include "datatypes.h"
 #include <stdio.h>
 
-#define A    0x00
-#define B    0x01
-#define PC   0x02
-#define ADD  0x02
-#define PCZ  0x03
-#define AND  0x03
-#define PCN  0x04
-#define OR   0x04
-#define PCC  0x05
-#define XOR  0x05
-#define PCZC 0x06
-#define RSH  0x06
-#define PCZD 0x07
-#define LIT  0x07
+#define A     0x00
+#define B     0x01
+#define PC    0x02
+#define ADD   0x02
+#define PCZ   0x03
+#define AND   0x03
+#define PCN   0x04
+#define OR    0x04
+#define PCC   0x05
+#define XOR   0x05
+#define PCZC  0x06
+#define RSH   0x06
+#define PCZD  0x07
+#define LIT   0x07
 #define CP    0x08
 #define C     0x09
 #define INC_C 0x0a
