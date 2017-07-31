e72bf91e46b6f234b65cf02c52005250a78b5895
Adly Abdullah <adly@trustedcompany.com>
2017-07-31 14:55:27 +0800

    Improve css

--- m516/js516/ui.js
+++ m516/js516/ui.js
@@ -3,6 +3,24 @@ function get (el) {
 	return el;
 }
 
-function MemList (arr) {
+function hexify (n) {
+	n = n.toString(16);
 	
+	while (n.length < 4) {
+		n = '0' + n;
+	}
+	
+	return n;
+}
+
+function memList (arr) {
+	var list = get('memory');
+
+	for (var x=0; x<1024; x++) {
+		list.innerHTML += '<div class="ram">' + hexify(x) + ' : 0000 </div>';
+	}
+}
+
+window.onload = function () {
+	memList();
 }
