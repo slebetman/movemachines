7cebe258f77f6bb47ac5ffe07972e62ad0f44d90
slebetman <slebetman@gmail.com>
2023-02-19 10:40:40 +0800

    Use module pattern for devices to hide implementation details

--- m516/web516/devices/device-manager.js
+++ m516/web516/devices/device-manager.js
@@ -1,87 +1,89 @@
-let devices = {
-	'7 Segment Display': {
-		init: () => led7seg(0x0200),
-		enabled: false,
-		device: null,
-	}
-}
-
-function listDevice (title, prop) {
-	let device = make('div',{
-		className: 'device-list'
-	});
-	device.innerHTML = title;
-
-	function close () {
-		prop.device.close();
-		prop.enabled = false;
-		prop.btn.innerText = 'Enable';
-		prop.btn.className = 'off';
+let deviceManager =(() => {
+	let devices = {
+		'7 Segment Display': {
+			init: () => led7seg(0x0200),
+			enabled: false,
+			device: null,
+		}
 	}
 
-	prop.btn = make('button',{
-		className: prop.enabled ? 'on' : 'off',
-		innerText: prop.enabled ? 'Disable' : 'Enable',
-		onclick: () => {
-			if (prop.enabled) {
-				close();
-			}
-			else {
-				prop.device = prop.init();
-				prop.device.cleanup = close;
-				prop.enabled = true;
-				prop.btn.innerText = 'Disable';
-				prop.btn.className = 'on';
-			}
-		}
-	});
+	function listDevice (title, prop) {
+		let device = make('div',{
+			className: 'device-list'
+		});
+		device.innerHTML = title;
 
-	device.appendChild(prop.btn);
-
-	return device;
-}
-
-function deviceManager () {
-	let interface = {};
-
-	toolWindow('Device Manager', 350, 150, `
-		<style>
-			#device-manager {
-				overflow: auto;
-			}
-			#device-manager .device-list button {
-				float: right;
-				margin-right: 5px;
-				border-radius: 3px;
-				border: 1px solid #777;
-			}
-			#device-manager button.on {
-				background-color: #333;
-				color: #fff;
-			}
-			#device-manager button.off {
-				background-color: #fff;
-				color: #000;
-			}
-		</style>
-		<div id="device-manager">
-
-		</div>
-	`,
-	(w) => {
-		let container = get('device-manager');
-
-		for (let d in devices) {
-			let el = listDevice(d, devices[d]);
-			container.appendChild(el);
+		function close () {
+			prop.device.close();
+			prop.enabled = false;
+			prop.btn.innerText = 'Enable';
+			prop.btn.className = 'off';
 		}
 
-		w.cleanup = () => {
-			if (interface.cleanup) {
-				interface.cleanup();
+		prop.btn = make('button',{
+			className: prop.enabled ? 'on' : 'off',
+			innerText: prop.enabled ? 'Disable' : 'Enable',
+			onclick: () => {
+				if (prop.enabled) {
+					close();
+				}
+				else {
+					prop.device = prop.init();
+					prop.device.cleanup = close;
+					prop.enabled = true;
+					prop.btn.innerText = 'Disable';
+					prop.btn.className = 'on';
+				}
 			}
-		}
-	})
+		});
 
-	return interface;
-}
+		device.appendChild(prop.btn);
+
+		return device;
+	}
+
+	return function () {
+		let interface = {};
+
+		toolWindow('Device Manager', 350, 150, `
+			<style>
+				#device-manager {
+					overflow: auto;
+				}
+				#device-manager .device-list button {
+					float: right;
+					margin-right: 5px;
+					border-radius: 3px;
+					border: 1px solid #777;
+				}
+				#device-manager button.on {
+					background-color: #333;
+					color: #fff;
+				}
+				#device-manager button.off {
+					background-color: #fff;
+					color: #000;
+				}
+			</style>
+			<div id="device-manager">
+
+			</div>
+		`,
+		(w) => {
+			let container = get('device-manager');
+
+			for (let d in devices) {
+				let el = listDevice(d, devices[d]);
+				container.appendChild(el);
+			}
+
+			w.cleanup = () => {
+				if (interface.cleanup) {
+					interface.cleanup();
+				}
+			}
+		})
+
+		return interface;
+	}
+})()
--- m516/web516/devices/led7seg.js
+++ m516/web516/devices/led7seg.js
@@ -33,123 +33,127 @@
 // 	F	0x1b
 // -----------------------------------------------
 
-let led7ON = '#000';
-let led7OFF = '#ddd';
+let led7seg = (() => {
+	let led7ON = '#000';
+	let led7OFF = '#ddd';
 
-function LED7draw (id) {
-	return `
-	<svg
-		width="34"
-		height="64"
-		viewBox="0 0 34 64"
-		id="led7seg-${id}"
-		class="led7seg"
-	>
-		<path
-			class="segment" style="stroke: ${led7OFF};"
-			d="M 8.2645,3.2645 H 26.298174"
-			id="led7seg-${id}-0" />
-		<path
-			class="segment" style="stroke: ${led7OFF};"
-			d="m 31.2645,26.2645 v -18"
-			id="led7seg-${id}-2" />
-		<path
-			class="segment" style="stroke: ${led7OFF};"
-			d="m 3.2645,26.2645 v -18"
-			id="led7seg-${id}-1" />
-		<path
-			class="segment" style="stroke: ${led7OFF};"
-			d="m 31.2645,56.2645 v -18"
-			id="led7seg-${id}-5" />
-		<path
-			class="segment" style="stroke: ${led7OFF};"
-			d="m 3.2645,56.2645 v -18"
-			id="led7seg-${id}-4" />
-		<path
-			class="segment" style="stroke: ${led7OFF};"
-			d="M 8.2645,32.264499 H 26.399955"
-			id="led7seg-${id}-3" />
-		<path
-			class="segment" style="stroke: ${led7OFF};"
-			d="M 8.2645,61.264501 H 26.399955"
-			id="led7seg-${id}-6" />
-	</svg>
-	`
-}
+	function LED7draw (id) {
+		return `
+		<svg
+			width="34"
+			height="64"
+			viewBox="0 0 34 64"
+			id="led7seg-${id}"
+			class="led7seg"
+		>
+			<path
+				class="segment" style="stroke: ${led7OFF};"
+				d="M 8.2645,3.2645 H 26.298174"
+				id="led7seg-${id}-0" />
+			<path
+				class="segment" style="stroke: ${led7OFF};"
+				d="m 31.2645,26.2645 v -18"
+				id="led7seg-${id}-2" />
+			<path
+				class="segment" style="stroke: ${led7OFF};"
+				d="m 3.2645,26.2645 v -18"
+				id="led7seg-${id}-1" />
+			<path
+				class="segment" style="stroke: ${led7OFF};"
+				d="m 31.2645,56.2645 v -18"
+				id="led7seg-${id}-5" />
+			<path
+				class="segment" style="stroke: ${led7OFF};"
+				d="m 3.2645,56.2645 v -18"
+				id="led7seg-${id}-4" />
+			<path
+				class="segment" style="stroke: ${led7OFF};"
+				d="M 8.2645,32.264499 H 26.399955"
+				id="led7seg-${id}-3" />
+			<path
+				class="segment" style="stroke: ${led7OFF};"
+				d="M 8.2645,61.264501 H 26.399955"
+				id="led7seg-${id}-6" />
+		</svg>
+		`
+	}
 
-function led7seg (addr) {
-	let interface = {}
+	return function (addr) {
+		let interface = {}
 
-	toolWindow('7 Segment Display', 210, 95, `
-		<style>
-			svg.led7seg path.segment {
-				fill:none;
-				stroke-width: 6px;
-				stroke-linecap:round;
-				stroke-miterlimit:10;
-				stroke-dasharray:none;
-			}
-
-			svg.led7seg {
-				display: inline-block;
-				margin: 5px;
-			}
-		</style>
-		<div id="led7seg">
-			${LED7draw(3)}
-			${LED7draw(2)}
-			${LED7draw(1)}
-			${LED7draw(0)}
-		</div>
-	`,
-	(w) => {
-		function led7write (id, val) {
-			for (let i=0; i<7; i++) {
-				let s = get(`led7seg-${id}-${i}`);
-				
-				if (val & (1 << i)) {
-					s.style.stroke = led7ON;
+		toolWindow('7 Segment Display', 210, 95, `
+			<style>
+				svg.led7seg path.segment {
+					fill:none;
+					stroke-width: 6px;
+					stroke-linecap:round;
+					stroke-miterlimit:10;
+					stroke-dasharray:none;
 				}
-				else {
-					s.style.stroke = led7OFF;
-				}
-			}
-		}
 
-		let cache = [0,0,0,0];
-		let updated = [false, false, false, false];
-		
-		for (let i=0; i<4; i++) {
-			attachMemDevice(addr+i,
-				null,
-				(val) => {
-					if (val != cache[i]) {
-						cache[i] = val;
-						updated[i] = true;
+				svg.led7seg {
+					display: inline-block;
+					margin: 5px;
+				}
+			</style>
+			<div id="led7seg">
+				${LED7draw(3)}
+				${LED7draw(2)}
+				${LED7draw(1)}
+				${LED7draw(0)}
+			</div>
+		`,
+		(w) => {
+			function led7write (id, val) {
+				for (let i=0; i<7; i++) {
+					let s = get(`led7seg-${id}-${i}`);
+					
+					if (val & (1 << i)) {
+						s.style.stroke = led7ON;
 					}
-				},
-				() => {
-					if (updated[i]) {
-						led7write(i, cache[i]);
-						updated[i] = false;
+					else {
+						s.style.stroke = led7OFF;
 					}
 				}
-			);
-		}
+			}
 
-		w.cleanup = () => {
+			let cache = [0,0,0,0];
+			let updated = [false, false, false, false];
+			
 			for (let i=0; i<4; i++) {
-				removeMemDevice(addr+i);
+				attachMemDevice(addr+i,
+					() => {
+						return cache[i];
+					},
+					(val) => {
+						if (val != cache[i]) {
+							cache[i] = val;
+							updated[i] = true;
+						}
+					},
+					() => {
+						if (updated[i]) {
+							led7write(i, cache[i]);
+							updated[i] = false;
+						}
+					}
+				);
 			}
-			if (interface.cleanup) {
-				interface.cleanup();
+
+			w.cleanup = () => {
+				for (let i=0; i<4; i++) {
+					removeMemDevice(addr+i);
+				}
+				if (interface.cleanup) {
+					interface.cleanup();
+				}
 			}
-		}
 
-		interface.close = w.close;
-		interface.write = led7write;
-	})
+			interface.close = w.close;
+			interface.write = led7write;
+		})
 
-	return interface;
-}
+		return interface;
+	}
 
+})()
