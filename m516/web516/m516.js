b7fd685c3db7867c7c9909c3be01b7259b97ab84
slebetman <slebetman@gmail.com>
2023-02-21 22:07:55 +0800

    Change stop key

--- m516/web516/m516.js
+++ m516/web516/m516.js
@@ -135,7 +135,7 @@ get('device-manager-btn').onclick = () => deviceManager();
 
 document.body.onkeyup = (e) => {
 	switch (e.key) {
-		case ' ': stop(); break;
+		case 'ArrowLeft': stop(); break;
 		case 'ArrowRight': run(); break;
 		case 'ArrowDown': step(); break;
 	}
